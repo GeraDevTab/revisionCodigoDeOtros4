e705f857b1b198a745aafc30f4ebe86eb111ec14
geradevtab <everythingissofast@gmail.com>
2023-03-29 23:13:44 -0600

    codigo corregido del juego de piedra, papel o tijera

--- Codigo4.java
+++ Codigo4.java
@@ -1,38 +1,78 @@
+package Codigo4;
+
+import java.util.Scanner;
+
 public class Codigo4 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		  Scanner s = new Scanner();
+			//agrega parametro System.in a la instanciacion del objeto Scanner
+		  	Scanner entrada = new Scanner(System.in);
 		    
 		    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-		    String j1 = s.nextLine();
 		    
-		    System.out.print("Turno del jugador 1 (introduzca piedra, papel o tijeras): ");
-		    Scanner s2 = new Scanner();
-		    String j2 = s.nextLine();
+		    //cqambio de nombre de variable de j1 a jugador1
+		    String jugador1 = entrada.nextLine();
 		    
-		    if (j1 == j2)) {
-		      System.out.println("Empate");
-		    } else {
-		      int g = 2;
-		      switch(j1) {
+		    //cambio en el numero del jugador 2, en el mensaje
+		    System.out.print("Turno del jugador 2 (introduzca piedra, papel o tijeras): ");
+		    
+		    //Scanner s2 = new Scanner(System.in);
+		    String jugador2 = entrada.nextLine();
+		    
+		    //se cambio a equals para la comparacion de los objetos envueltos de las datos primitivos
+		    if (jugador1.equals(jugador2)) {
+		    	//se movio la line de impresion siguiente dentro de las llaves del if
+		    	System.out.println(jugador1);
+		    	System.out.println(jugador2);
+		    	System.out.println("Empate");
+		    	
+		    }
+		    //se elimina una llave
+		    else {
+		    	//System.out.println(jugador1);
+		    	//System.out.println(jugador2);
+		    	//cambio de nombre de variable de g a ganador
+		    int ganador = 2;
+		    
+		    switch(jugador1)
+		    {
 		        case "piedra":
-		          if (j2 == "tijeras") {
-		            g = 1;
+		          if (jugador2.equals("tijeras")) {
+		            ganador = 1;
+		            break;
 		          }
-
+		          else if(jugador2.equals("papel")) {
+			            ganador = 2;
+			            break;
+		          }
 		        case "papel":
-		          if (j2 == "piedra") {
-		            g = 1;
-		          
-		        case "tijera":
-		          if (j2.equals("papel")) {
-		            g = 1;
+		          if (jugador2.equals("piedra")) {
+		            ganador = 1;
+		            break;
 		          }
-		          break;
-		        default:
+		    
+		          else if(jugador2.equals("tijera")) {
+		            ganador = 2;
+		            break;
+		          }
+		        case "tijera":
+		          if (jugador2.equals("papel")) {
+		            ganador = 1;
+		            break;
+		          }
+		          else if(jugador2.equals("piedra")) {
+			         ganador = 2;
+			         break;
+			      }
+		        default:{
+		        	System.out.println("ha ingresado un dato que no permite, definir el ganador :D");
+		        	ganador = 0;
+		        }
 		      }
-		      System.out.println("Gana el jugador " + g);
+		      System.out.println("Gana el jugador " + ganador);
 		    }
 	}
+
+}
